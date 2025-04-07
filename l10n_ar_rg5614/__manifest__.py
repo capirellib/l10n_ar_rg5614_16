dccb9f6598ddd6c41e401b19dd46bfba861fce33
Aliaresrl <143626551+Aliaresrl@users.noreply.github.com>
2025-04-07 10:27:53 -0300

    Update __manifest__.py

--- l10n_ar_rg5614/__manifest__.py
+++ l10n_ar_rg5614/__manifest__.py
@@ -13,7 +13,7 @@
     "assets": {
         "web.assets_backend": [],
         "web.assets_qweb": [
-            "l10n_ar_5614/static/src/xml/pos_tickets.xml",
+            "l10n_ar_rg5614/static/src/xml/pos_tickets.xml",
         ],
     },
     "application": False,
